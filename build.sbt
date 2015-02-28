61ae0ee70d2d0297d6c634aadc4832561dd6f15b
tmperret <timothy.m.perrett@oncue.com>
2015-02-27 20:43:27 -0800

    adding example

---
+++ build.sbt
@@ -0,0 +1,15 @@
+
+organization := "com.timperrett"
+
+name := "scalaz-stream-websockets"
+
+scalaVersion := "2.11.4"
+
+libraryDependencies ++= Seq(
+  "org.scalaz.stream" %% "scalaz-stream"               % "0.6a",
+  "net.databinder"    %% "unfiltered-filter"           % "0.8.4",
+  "net.databinder"    %% "unfiltered-netty-server"     % "0.8.4",
+  "net.databinder"    %% "unfiltered-netty-websockets" % "0.8.4"
+)
+
+resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
---
+++ src/main/scala/main.scala
@@ -0,0 +1,52 @@
+package example
+
+import unfiltered.request._
+import unfiltered.response._
+import unfiltered.netty._
+import unfiltered.netty.websockets._
+import scalaz.stream.Process
+import scalaz.concurrent.{Task,Strategy}
+import scala.concurrent.duration._
+import java.util.concurrent.{Executors, ExecutorService, ScheduledExecutorService, ThreadFactory}
+
+object Server {
+
+  val serverPool: ExecutorService =
+    Executors.newCachedThreadPool(daemonThreads("server"))
+
+  val schedulingPool: ScheduledExecutorService =
+    Executors.newScheduledThreadPool(2, daemonThreads("scheduled-tasks"))
+
+  private def daemonThreads(name: String) = new ThreadFactory {
+    def newThread(r: Runnable) = {
+      val t = Executors.defaultThreadFactory.newThread(r)
+      t.setDaemon(true)
+      t.setName(name)
+      t
+    }
+  }
+
+  val stream: Process[Task,Long] =
+    Process.awakeEvery(1.seconds)(Strategy.Executor(serverPool),schedulingPool)
+           .map(_ => System.currentTimeMillis)
+
+  def main(args: Array[String]): Unit = {
+    unfiltered.netty.Server
+      .http(8080)
+      .handler(Example(stream))
+      .run
+  }
+}
+
+@io.netty.channel.ChannelHandler.Sharable
+case class Example(proc: Process[Task,Long]) extends websockets.Plan with cycle.SynchronousExecution with ServerErrorResponse {
+  val pass: PassHandler = DefaultPassHandler
+  def intent = {
+    case GET(Path("/stream")) => {
+      case Open(s) => proc.evalMap(long => s.send(long.toString))
+      // case Message(s, Text(str)) => sockets.foreach(_.send(str.reverse))
+      // case Close(s)              => sockets -= s
+      // case Error(s, e)           => println("error %s" format e.getMessage)
+    }
+  }
+}
